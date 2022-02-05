8d001c5899c5a680835ca002fb58f08840a0b22a
francy <francy>
2022-02-05 16:07:16 +0100

    ability to use toolbar component in groovy script

--- resources/views/Summary/summaryScript.groovy
+++ resources/views/Summary/summaryScript.groovy
@@ -1,9 +1,4 @@
 
-
-//EBICRMToolBar.toolbarItemTerminalModule.actionPerformed = { println "hallo world";}
-
-
-
 println "Summary type label is:"+Summary.summaryType.text;
 
 println ebiToolBar.toolbarItemTerminalModule.toolTipText;
